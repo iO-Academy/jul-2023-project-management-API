7720c81bccdcf368cf4b34f100fee49af18df7e6
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-09-11 16:01:09 +0100

    PR changes

--- src/Hydrators/ProjectsHydrator.php
+++ src/Hydrators/ProjectsHydrator.php
@@ -2,11 +2,14 @@
 
 namespace ProjectManager\Hydrators;
 
+use ProjectManager\Entities\ProjectEntity;
+
 class ProjectsHydrator {
-    public static function getProjects(PDO $db) {
+    public static function getProjects(\PDO $db)
+    {
         $query = $db->prepare('SELECT `id`, `name`, `client_id`, `deadline` FROM `projects`');
         $query->execute();
-        $query->setFetchMode(PDO::FETCH_CLASS, ProjectEntity::class);
+        $query->setFetchMode(\PDO::FETCH_CLASS, ProjectEntity::class);
         return $query->fetchAll();
     }
 }
