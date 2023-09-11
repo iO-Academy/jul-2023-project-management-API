acd122cf6bc4802075c7226ff7cd45a235c9a08c
Cosmin <cosmin_7d@yahoo.com>
2023-09-11 16:35:20 +0100

    pr review changes made

--- src/Services/ConvertToJsonService.php
+++ src/Services/ConvertToJsonService.php
@@ -5,7 +5,7 @@ class ConvertToJsonService
     public static function convert(array $data):string
     {
         return json_encode($data);
-        
+
         //below is for task 5
 //        if(json_encode($data)) {
 //            return http_response_code(200);
