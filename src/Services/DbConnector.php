b061b2c1f5a6440ea698d3dc4d4983df0963d1f7
MrTKetchup <alexanderjamesmansfield@gmail.com>
2023-09-11 15:39:01 +0100

    db connection made with errmode and fetchmode attributes set

---
+++ src/Services/DbConnector.php
@@ -0,0 +1,13 @@
+<?php
+namespace ProjectManager\Services;
+
+class DbConnector {
+    public static function connect(): \PDO
+    {
+        $db = new \PDO('mysql:dbname=project-manager;host=db', 'root', 'password');
+        $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $db->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+        return $db;
+    }
+}
+
