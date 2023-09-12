0a5d284289f516516cffd92856d7337f406d06a5
Cosmin <cosmin_7d@yahoo.com>
2023-09-12 13:52:24 +0100

    Better naming for constants successfull message variables

--- projects.php
+++ projects.php
@@ -3,7 +3,7 @@ header('Content-Type: application/json; charset=utf-8');
 require 'vendor/autoload.php';
 use \ProjectManager\Services\ConvertToJsonService;
 
-echo ConvertToJsonService::convert([NAN], 1);
+echo ConvertToJsonService::convert([], ConvertToJsonService::PROJECTS_SUCCESS_MESSAGE);
 $db = \ProjectManager\Services\DbConnector::connect();
-\ProjectManager\Services\ConvertToJsonService::convert($data);
+
 
