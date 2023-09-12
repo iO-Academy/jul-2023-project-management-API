4feae85473cfb1482a209e64a9e20e4d5f8fb1af
Cosmin <cosmin_7d@yahoo.com>
2023-09-12 11:45:16 +0100

    changes made according to PR

--- tests/ConvertToJsonServiceTest.php
+++ tests/ConvertToJsonServiceTest.php
@@ -3,20 +3,21 @@ use PHPUnit\Framework\TestCase;
 
 class ConvertToJsonServiceTest extends TestCase
 {
-    public  function testConvert_success()
+    public function testConvert_success()
     {
         $result = \ProjectManager\Services\ConvertToJsonService::convert([1,2,3,4],0);
         $expected = '{"message":"Successfully retrieved projects","data":[1,2,3,4]}';
         $this->assertSame($expected,$result);
     }
-    public  function testConvert_success_unexpected_error()
+
+    public function testConvert_success_unexpected_error()
     {
         $result = \ProjectManager\Services\ConvertToJsonService::convert([NAN],0);
         $expected = '{"message":"Unexpected error","data":[]}';
         $this->assertSame($expected,$result);
     }
 
-    public  function testConvert_failure()
+    public function testConvert_failure()
     {
         $this->expectException(Exception::class);
         \ProjectManager\Services\ConvertToJsonService::convert([1,2,3,4],100);
