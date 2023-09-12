8721c0943a0042f5ae488ffde46fadb0d298f632
Cosmin <cosmin_7d@yahoo.com>
2023-09-12 12:02:29 +0100

    created Services directory in tests directory

--- tests/Services/ConvertToJsonServiceTest.php
+++ tests/Services/ConvertToJsonServiceTest.php
@@ -1,37 +1,40 @@
 <?php
+
+namespace Services;
+
 use PHPUnit\Framework\TestCase;
 
 class ConvertToJsonServiceTest extends TestCase
 {
     public function testConvert_success()
     {
-        $result = \ProjectManager\Services\ConvertToJsonService::convert([1,2,3,4],0);
+        $result = \ProjectManager\Services\ConvertToJsonService::convert([1, 2, 3, 4], 0);
         $expected = '{"message":"Successfully retrieved projects","data":[1,2,3,4]}';
-        $this->assertSame($expected,$result);
+        $this->assertSame($expected, $result);
     }
 
     public function testConvert_success_unexpected_error()
     {
-        $result = \ProjectManager\Services\ConvertToJsonService::convert([NAN],0);
+        $result = \ProjectManager\Services\ConvertToJsonService::convert([NAN], 0);
         $expected = '{"message":"Unexpected error","data":[]}';
-        $this->assertSame($expected,$result);
+        $this->assertSame($expected, $result);
     }
 
     public function testConvert_failure()
     {
         $this->expectException(Exception::class);
-        \ProjectManager\Services\ConvertToJsonService::convert([1,2,3,4],100);
+        \ProjectManager\Services\ConvertToJsonService::convert([1, 2, 3, 4], 100);
     }
 
     public function testConvert_malformed_data()
     {
         $this->expectException(TypeError::class);
-        \ProjectManager\Services\ConvertToJsonService::convert('',0);
+        \ProjectManager\Services\ConvertToJsonService::convert('', 0);
     }
 
     public function testConvert_malformed_message()
     {
         $this->expectException(TypeError::class);
-        \ProjectManager\Services\ConvertToJsonService::convert([],'');
+        \ProjectManager\Services\ConvertToJsonService::convert([], '');
     }
 }
