6fcb943ab720a625cf480bde2fb6711534957a6b
MrTKetchup <alexanderjamesmansfield@gmail.com>
2023-09-12 12:22:30 +0100

    copied and combined the DateService Tests into one file

--- tests/Services/DateServiceTest.php
+++ tests/Services/DateServiceTest.php
@@ -4,6 +4,24 @@ use PHPUnit\Framework\TestCase;
 
 class DateServiceTest extends TestCase
 {
+    public function testConvertToUkFormat_success()
+    {
+        $unformattedDate = '2024-03-08';
+        $result = \ProjectManager\Services\DateService::convertToUkFormat($unformattedDate);
+        $this->assertSame('08/03/2024', $result);
+    }
+
+    public function testConvertToUkFormat_malformed()
+    {
+        $this->expectException(TypeError::class);
+        \ProjectManager\Services\DateService::convertToUkFormat([]);
+    }
+
+    public function testConvertToUkFormat_failure()
+    {
+        $this->expectException(Exception::class);
+        \ProjectManager\Services\DateService::convertToUkFormat('2023-15-03');
+    }
     public function testIsOverdue_success_true()
     {
         $date = '01/01/1970';
