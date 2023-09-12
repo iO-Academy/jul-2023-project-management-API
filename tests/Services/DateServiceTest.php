7ab333f61686fb31919d28228a2e561c623c7bf2
MrTKetchup <alexanderjamesmansfield@gmail.com>
2023-09-12 10:42:21 +0100

    ready for PR

--- tests/Services/DateServiceTest.php
+++ tests/Services/DateServiceTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 
 class DateServiceTest extends TestCase
 {
-    public function testIsOverdueTrue()
+    public function testIsOverdueTrueSuccess()
     {
         $date = '01/01/1970';
         $result = DateService::isOverdue($date);
@@ -15,7 +15,7 @@ class DateServiceTest extends TestCase
         $this->assertSame(true, $result);
     }
 
-    public function testIsOverdueFalse()
+    public function testIsOverdueFalseSuccess()
     {
         $date = '01/01/3000';
         $result = DateService::isOverdue($date);
