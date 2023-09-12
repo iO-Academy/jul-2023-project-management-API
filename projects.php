89fb7773fa616f702b7e8e35f095b99e2204ddf3
iO Academy <thejoshsilasbryan@gmail.com>
2023-09-12 14:53:20 +0100

    Ready for peer review

--- projects.php
+++ projects.php
@@ -4,14 +4,8 @@ header("Access-Control-Allow-Origin: *");
 require 'vendor/autoload.php';
 use \ProjectManager\Services\ConvertToJsonService;
 
-<<<<<<< HEAD
-echo ConvertToJsonService::convert([], ConvertToJsonService::PROJECTS_SUCCESS_MESSAGE);
-$db = \ProjectManager\Services\DbConnector::connect();
-
-=======
 $db = \ProjectManager\Services\DbConnector::connect();
 $data = \ProjectManager\Hydrators\ProjectsHydrator::getProjects($db);
-$jsonData = \ProjectManager\Services\ConvertToJsonService::convert($data, ConvertToJsonService::PROJECTS_URL);
->>>>>>> 86db27cc296b060b518510083e61a3590458e3d2
+$jsonData = \ProjectManager\Services\ConvertToJsonService::convert($data,ConvertToJsonService::PROJECTS_SUCCESS_MESSAGE);
 
 echo $jsonData;
