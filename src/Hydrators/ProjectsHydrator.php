429841dcdcc4665587cb06497c1d7d95e37ef6e0
Cosmin <cosmin_7d@yahoo.com>
2023-09-12 15:47:39 +0100

    created getProject and getUsersByProjectId methods'

--- src/Hydrators/ProjectsHydrator.php
+++ src/Hydrators/ProjectsHydrator.php
@@ -2,6 +2,7 @@
 
 namespace ProjectManager\Hydrators;
 
+use PhpParser\Node\Expr\Array_;
 use ProjectManager\Entities\ProjectEntity;
 
 class ProjectsHydrator {
@@ -11,5 +12,19 @@ class ProjectsHydrator {
         $query->execute();
         $query->setFetchMode(\PDO::FETCH_CLASS, ProjectEntity::class);
         return $query->fetchAll();
+    }
+    public static function getProject(\PDO $db, int $projectId)
+    {
+        $query = $db->prepare('SELECT `id`,`name`,`client_id`,`deadline` FROM `projects` WHERE `id` = ?');
+        $query->execute([$projectId]);
+        $query->setFetchMode(\PDO::FETCH_CLASS,SpecificProjectEntity::class);
+        return $query->fetch();
+
+    }
+    private static function getUsersByProjectId(\PDO $db, int $projectId)
+    {
+        $query = $db->prepare('SELECT `id`,`name`,`avatar`,`role` FROM `projects` WHERE `id` = ?');
+        $query->execute([$projectId]);
+        $query->setFetchMode(\PDO::FETCH_CLASS,UserEntity::class);
     }
 }
