57d23a5d8fc1dc2aeb50c35e717240a29501a387
iO Academy <thejoshsilasbryan@gmail.com>
2023-09-12 16:48:41 +0100

    Ready for Trainer review 2

--- src/Entities/ProjectEntity.php
+++ src/Entities/ProjectEntity.php
@@ -11,21 +11,6 @@ class ProjectEntity implements \JsonSerializable {
     private int $client_id;
     private ?string $deadline;
 
-    public function getId(): int
-    {
-        return $this->id;
-    }
-
-    public function getName(): string
-    {
-        return $this->name;
-    }
-
-    public function getClientId(): int
-    {
-        return $this->client_id;
-    }
-
     public function getDeadline(): ?string
     {
         return $this->deadline ? DateService::convertToUkFormat($this->deadline) : null;
@@ -35,7 +20,6 @@ class ProjectEntity implements \JsonSerializable {
     {
         return $this->deadline ? DateService::isOverdue($this->deadline) : null;
     }
-
 
     public function jsonSerialize(): array
     {
