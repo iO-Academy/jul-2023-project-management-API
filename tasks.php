bf1445a3b3fa17478ebece9b19bc251f52fd2db2
iO Academy <thejoshsilasbryan@gmail.com>
2023-09-13 16:36:20 +0100

    Can handle errors for valid projects id and user id needs user tests: '

--- tasks.php
+++ tasks.php
@@ -4,18 +4,20 @@ header("Access-Control-Allow-Origin: *");
 require 'vendor/autoload.php';
 
 try {
-$db = \ProjectManager\Services\DbConnector::connect();
-$data = \ProjectManager\Hydrators\TasksHydrator::getTasksByUserAndProjectId($db);
-    if (is_int(['project_id']) && ['project_id'] > 0) {
-        $jsonData = \ProjectManager\Services\ConvertToJsonService::convert($data, \ProjectManager\Services\ConvertToJsonService::TASK_SUCCESS_MESSAGE);
-        if (empty($jsonData)){
-            \ProjectManager\Services\ConvertToJsonService::NoTasksAssignedToThatUserErrorResponse();
-        }
+    $db = \ProjectManager\Services\DbConnector::connect();
+    $data = \ProjectManager\Hydrators\TasksHydrator::getTasksByUserAndProjectId($db);
+        if (is_int(['project_id']) && ['project_id'] > 0 && is_int(['user_id']) && ['user_id'] > 0) {
+        $jsonData = \ProjectManager\Hydrators\TasksHydrator::getTasksByUserAndProjectId($db);
     } else {
-        \ProjectManager\Services\ConvertToJsonService::invalidProjectIdResponse();
+        if (!is_int(['project_id']) || ['project_id'] <= 0) {
+            $jsonData = \ProjectManager\Services\ConvertToJsonService::invalidProjectIdResponse();
+            echo $jsonData;
+        }
+        if (!is_int(['user_id']) || ['user_id'] <= 0) {
+            $jsonData = \ProjectManager\Services\ConvertToJsonService::invalidUserIdResponse();
+            echo $jsonData;
+        }
     }
-    } catch (Exception $e) {
-        echo \ProjectManager\Services\ConvertToJsonService::unexpectedErrorResponse();
-    }
-
-
+} catch (Exception $e) {
+       echo \ProjectManager\Services\ConvertToJsonService::unexpectedErrorResponse();
+  }
