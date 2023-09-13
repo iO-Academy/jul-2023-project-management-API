8a08041eeea6fe41e3bfb0e92edd4f31d8ee3fea
iO Academy <colinrmeier@gmail.comgit config --global user.name Colin>
2023-09-13 10:25:01 +0100

    Added some unit tests

---
+++ tests/Entities/SpecificProjectEntityTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Entities;
+
+use PHPUnit\Framework\TestCase;
+
+class UserEntityTest extends TestCase
+{
+    public function testGetClientName()
+    {
+        $this->markTestSkipped('Skipping due to no logic');
+    }
+
+    public function testGetClientLogo()
+    {
+        $this->markTestSkipped('Skipping due to no logic');
+    }
+
+    public function testSetUsers_success()
+    {
+
+    }
+}
---
+++ tests/Entities/UserEntityTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Entities;
+
+use PHPUnit\Framework\TestCase;
+
+class UserEntityTest extends TestCase
+{
+    public function testGetId()
+    {
+        $this->markTestSkipped('Skipping due to no logic');
+    }
+
+    public function testGetName()
+    {
+        $this->markTestSkipped('Skipping due to no logic');
+    }
+
+    public function testGetAvatar()
+    {
+        $this->markTestSkipped('Skipping due to no logic');
+    }
+
+    public function testGetRole()
+    {
+        $this->markTestSkipped('Skipping due to no logic');
+    }
+}
