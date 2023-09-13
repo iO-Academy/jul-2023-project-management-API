176be89282936a30bbe1f67e36cf0f41fc94b181
iO Academy <thejoshsilasbryan@gmail.com>
2023-09-13 12:16:37 +0100

    Tasks.php made"

---
+++ Tasks.php
@@ -0,0 +1,9 @@
+<?php
+header('Content-Type: application/json; charset=utf-8');
+header("Access-Control-Allow-Origin: *");
+require 'vendor/autoload.php';
+
+$db = \ProjectManager\Services\DbConnector::connect();
+$data = \ProjectManager\Hydrators\TasksHydrator::getTasksByUserAndProjectId($db);
+
+
