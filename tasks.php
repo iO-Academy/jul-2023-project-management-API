896f3646cbaaa7eaa2305355552b6d0cd4546ded
iO Academy <thejoshsilasbryan@gmail.com>
2023-09-14 12:30:28 +0100

    .

--- tasks.php
+++ tasks.php
@@ -3,28 +3,27 @@ header('Content-Type: application/json; charset=utf-8');
 header("Access-Control-Allow-Origin: *");
 require 'vendor/autoload.php';
 
-//try {
-
+try {
     $db = \ProjectManager\Services\DbConnector::connect();
     $data = \ProjectManager\Hydrators\TasksHydrator::getTasksByUserAndProjectId($db, $_GET['project_id'], $_GET['user_id']);
 
     $jsonData = \ProjectManager\Services\ConvertToJsonService::convert($data, \ProjectManager\Services\ConvertToJsonService::TASK_SUCCESS_MESSAGE);
     echo $jsonData;
-//    if (is_numeric(['project_id']) && ['project_id'] > 0 && is_numeric(['user_id']) && ['user_id'] > 0) {
-//        $jsonData = \ProjectManager\Hydrators\TasksHydrator::getTasksByUserAndProjectId($db);
-//    } else {
-//        if (!is_numeric(['project_id']) || ['project_id'] <= 0) {
-//            $jsonData = \ProjectManager\Services\ConvertToJsonService::invalidProjectIdResponse();
-//        }
-//        if (!is_numeric(['user_id']) || ['user_id'] <= 0) {
-//            $jsonData = \ProjectManager\Services\ConvertToJsonService::invalidUserIdResponse();
-//        }
-//        if (['user_id'] != ['project_id']) {
-//            $jsonData= \ProjectManager\Services\ConvertToJsonService::noTasksAssignedToUserErrorResponse();
-//        }
-//    }
-//    echo $jsonData;
-//}
-//catch (Exception $e) {
-//       echo \ProjectManager\Services\ConvertToJsonService::unexpectedErrorResponse();
-//  }
+    if (is_numeric(['project_id']) && ['project_id'] > 0 && is_numeric(['user_id']) && ['user_id'] > 0) {
+        $jsonData = \ProjectManager\Hydrators\TasksHydrator::getTasksByUserAndProjectId($db);
+    } else {
+        if (!is_numeric(['project_id']) || ['project_id'] <= 0) {
+            $jsonData = \ProjectManager\Services\ConvertToJsonService::invalidProjectIdResponse();
+        }
+        if (!is_numeric(['user_id']) || ['user_id'] <= 0) {
+            $jsonData = \ProjectManager\Services\ConvertToJsonService::invalidUserIdResponse();
+        }
+        if (['user_id'] != ['project_id']) {
+            $jsonData= \ProjectManager\Services\ConvertToJsonService::noTasksAssignedToUserErrorResponse();
+        }
+    }
+    echo $jsonData;
+}
+catch (Exception $e) {
+       echo \ProjectManager\Services\ConvertToJsonService::unexpectedErrorResponse();
+  }
