80e10c5d83a0e6473c2dfcd4452815b161e9e0bf
MrTKetchup <alexanderjamesmansfield@gmail.com>
2023-09-14 11:59:06 +0100

    FE loads correctly. tasks.php echoes the jsonData

--- src/Hydrators/TasksHydrator.php
+++ src/Hydrators/TasksHydrator.php
@@ -6,12 +6,12 @@ use ProjectManager\Entities\TaskEntity;
 
 class TasksHydrator
 {
-    public static function getTasksByProjectId(\PDO $db, int $projectId): array
+    public static function getTasksByProjectId(\PDO $db, int $projectId)
     {
         $query = $db->prepare('SELECT `id`, `project_id`, `user_id`, `name`, `estimate`, `deadline` FROM `tasks` WHERE `project_id` = ?');
         $query->execute([$projectId]);
         $query->setFetchMode(\PDO::FETCH_CLASS, TaskEntity::class);
-        return $query->fetch();
+        return $query->fetchAll();
     }
 
 //    public static function getTasksByUserId(\PDO $db, int $userId): array
