1348880cce4e07f3d27828fd3a17a08ee5b8d921
MrTKetchup <alexanderjamesmansfield@gmail.com>
2023-09-14 15:11:08 +0100

    unit tests included (hint there are none)

--- tasks.php
+++ tasks.php
@@ -7,7 +7,7 @@ require 'vendor/autoload.php';
     $db = \ProjectManager\Services\DbConnector::connect();
     $data = \ProjectManager\Hydrators\TasksHydrator::getTasksByUserAndProjectId($db, $_GET['project_id'], $_GET['user_id']);
 
-    $jsonData = \ProjectManager\Services\ConvertToJsonService::convert($data, \ProjectManager\Services\ConvertToJsonService::TASK_SUCCESS_MESSAGE);
+    $jsonData = \ProjectManager\Services\ConvertToJsonService::convert($data, \ProjectManager\Services\ConvertToJsonService::TASKS_SUCCESS_MESSAGE);
     echo $jsonData;
 //    if (is_numeric(['project_id']) && ['project_id'] > 0 && is_numeric(['user_id']) && ['user_id'] > 0) {
 //        $jsonData = \ProjectManager\Hydrators\TasksHydrator::getTasksByUserAndProjectId($db);
