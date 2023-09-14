aa1fd99b747760d12bc7f8e1281f3701e3026a25
MrTKetchup <alexanderjamesmansfield@gmail.com>
2023-09-14 11:09:52 +0100

    hydrator searches db and returns tasks assigned to project

--- src/Hydrators/TasksHydrator.php
+++ src/Hydrators/TasksHydrator.php
@@ -6,11 +6,19 @@ use ProjectManager\Entities\TaskEntity;
 
 class TasksHydrator
 {
-    public static function getTasksByUserAndProjectId(\PDO $db, int $projectId, int $userId): array
+    public static function getTasksByProjectId(\PDO $db, int $projectId): array
     {
-        $query = $db->prepare('SELECT `id`, `project_id`, `user_id`, `name`, `estimate`, `deadline` FROM `tasks`');
-        $query->execute([$projectId, $userId]);
+        $query = $db->prepare('SELECT `id`, `project_id`, `user_id`, `name`, `estimate`, `deadline` FROM `tasks` WHERE `project_id` = ?');
+        $query->execute([$projectId]);
         $query->setFetchMode(\PDO::FETCH_CLASS, TaskEntity::class);
         return $query->fetch();
     }
+
+//    public static function getTasksByUserId(\PDO $db, int $userId): array
+//    {
+//        $query = $db->prepare('SELECT `id`, `project_id`, `user_id`, `name`, `estimate`, `deadline` FROM `tasks` WHERE `user_id` = ?');
+//        $query->execute([$userId]);
+//        $query->setFetchMode(\PDO::FETCH_CLASS, TaskEntity::class);
+//        return $query->fetch();
+//    }
 }
